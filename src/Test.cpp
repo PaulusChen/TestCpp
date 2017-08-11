2d089a6d8133dc8925ce6e85ec075629cfef6030
chenpeng <chenpengsmail@qq.com>
2017-08-11 18:29:34 +0800

    udpate

--- src/Test.cpp
+++ src/Test.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdint.h>
+#include <vector>
 
 using namespace std;
 
@@ -55,20 +56,21 @@ constexpr int TestVal = TestFun(2);
 
 int *TestScanBuild() {
     int testVal;
+    testVal ^= testVal;
     return &testVal;
 }
 
 
 void TestVTab() {
-    int reval = TestFun(TestVal);
+    //int reval = TestFun(TestVal);
 
     TestClass *pPlus1 = new TestPlusClass(0x11111111);
     TestClass *pPlus2 = new TestPlusClass(0x22222222);
     TestClass *pMinus = new TestMinusClass;
 
-    int (TestClass::*pFunc1)(int a, int b) = &TestClass::TestFunc;
-    int (TestPlusClass::*pFunc2)(int a, int b) = &TestPlusClass::TestFunc;
-    int (TestMinusClass::*pFunc3)(int a, int b) = &TestMinusClass::TestFunc;
+    // int (TestClass::*pFunc1)(int a, int b) = &TestClass::TestFunc;
+    // int (TestPlusClass::*pFunc2)(int a, int b) = &TestPlusClass::TestFunc;
+    // int (TestMinusClass::*pFunc3)(int a, int b) = &TestMinusClass::TestFunc;
 
     int a{100};
     int b = 50;
@@ -81,7 +83,22 @@ void TestVTab() {
 
 }
 
+void user(const vector<int> &vi)
+{
+    constexpr int bufmax = 1024;
+    char buffer2[bufmax];
+    alignas(int) char buffer[bufmax];
+    auto ai = alignof(buffer);
+    auto ai2 = alignof(buffer2);
+    cout<<"alignof(buffer) : "<<ai<<" buffer addr : "<<ios_base::hex<<buffer<<" alignof(buffer2) : "<<static_cast<uint64_t>(ai2)<<" buffer2 addr : "<<ios_base::hex<<buffer2<<endl;
+
+    const int max = min(vi.size(), bufmax / sizeof(int));
+    uninitialized_copy(vi.begin(), vi.begin() + max, buffer);
+}
+
 int main(int argc, char *argv[]) {
+
+    user(vector<int>{1,2,3,4,5});
     return 0;
 }
 
