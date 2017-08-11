5cde9c6c89beb68966e143b28b77b45a29ae5a29
chenpeng <chenpengsmail@qq.com>
2017-08-11 16:08:25 +0800

    update automake

--- automake.sh
+++ automake.sh
@@ -10,26 +10,6 @@ if test ! -e SysLocLibInclude; then
 fi
 PROJECT_HOME=`pwd`
 
-if test ! -e thirdparty; then
-    mkdir thirdparty
-fi
-
-cd thirdparty
-
-
-# Check that gmock is present.  Usually it is already there since the
-# directory is set up as an SVN external.
-if test ! -e gtest; then
-    echo "Google Mock not present.  Fetching gmock-1.7.0 from the web..."
-    curl $curlopts -L -O https://codeload.github.com/google/googletest/zip/release-1.8.0
-    unzip -q release-1.8.0
-    rm release-1.8.0
-    mv googletest-release-1.8.0 gtest
-    cd gtest
-    cmake .
-    make
-fi
-
 cd ${PROJECT_HOME}
 
 ProjectRootDir=`pwd`
