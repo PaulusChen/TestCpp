7bda850d85f3e8dd9446ea7a745a7d18bb281188
chenpeng <chenpengsmail@qq.com>
2017-08-22 17:57:51 +0800

    update

--- src/Test.cpp
+++ src/Test.cpp
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <vector>
 #include <typeinfo>
+#include <cstring>
 
 using namespace std;
 
@@ -55,13 +56,6 @@ constexpr int TestFun(int testval) {
 
 constexpr int TestVal = TestFun(2);
 
-int *TestScanBuild() {
-    int testVal;
-    testVal ^= testVal;
-    return &testVal;
-}
-
-
 void TestVTab() {
     //int reval = TestFun(TestVal);
 
@@ -80,7 +74,6 @@ void TestVTab() {
     cout<<"a plus b equel : "<<pPlus2->TestFunc(a, b)<<endl;
 
     cout<<"a minus b equel : "<<pMinus->TestFunc(a,b)<<endl;
-    cout<<*TestScanBuild()<<endl;
 
 }
 
@@ -129,9 +122,126 @@ void user(const vector<int> &vi)
     using testtype = unsigned int;
 }
 
-int main(int argc, char *argv[]) {
 
-    user(vector<int>{1,2,3,4,5});
+template<typename T>
+int testfunc2(T t) {
+    cout<<"typeid of lambda : "<<typeid(t).name()<<endl;
+    t();
     return 0;
 }
 
+template<typename T>
+int testfunc(T t) {
+    char *oldname = (char *)typeid(t).name();
+    // strcpy(oldname,"cpmark");
+    testfunc2(t);
+    return 0;
+}
+
+class TestConstructor
+{
+   public:
+    //! Default constructor
+    TestConstructor() {
+        cout<<"default constructor"<<endl;
+    }
+
+    //! Copy constructor
+    TestConstructor(const TestConstructor &other);
+
+    //! Move constructor
+    TestConstructor(TestConstructor &&other) noexcept;
+
+    //! Destructor
+    virtual ~TestConstructor() noexcept;
+
+    //! Copy assignment operator
+    TestConstructor &operator=(const TestConstructor &other);
+
+    //! Move assignment operator
+    TestConstructor &operator=(TestConstructor &&other) noexcept;
+
+   protected:
+   private:
+};
+
+
+//user(vector<int>{1,2,3,4,5});
+//testfunc([](){ cout<<"test lambda"<<endl; });
+
+auto const testValA = 100;
+auto constexpr testValB = 100;
+
+int use2() {
+    cout << "type name : " << typeid(decltype(testValA)).name() << endl;
+    auto x = decltype(testValA){10};
+    cout << "type name : " << typeid(decltype(x + 100)).name()
+         << " the value is " << x << endl;
+
+    int a = 100;
+    int &ra = a;
+    auto autora = ra;
+    cout << "Is lvalue reference: " << is_lvalue_reference<decltype(autora)>::value << endl;
+    auto &autorra = ra;
+    cout << "Is lvalue reference: " << is_lvalue_reference<decltype(autorra)>::value << endl;
+
+    auto autoval1{100};
+    cout<<"type of autoval1 : "<<typeid(autoval1).name()<<endl;
+    auto autoval2 = {100};
+    cout<<"type of autoval2 : "<<typeid(autoval2).name()<<endl;
+    auto autoval3 = {100, 200, 300}; //error :  direct-list-initialization of ‘auto’ requires exactly one element [-fpermissive]
+    cout << "type of autoval3 : " << typeid(autoval3).name() << endl;
+
+    using Char2 = char;
+    //unsigned Char2 testval; //error: expected initializer before ‘testval’
+    //using Uchar = unsigned char;
+
+    // int *test = static_cast<int *>(123); // invalid static_cast from type ‘int’ to type ‘int*’
+    int *test = static_cast<int *>((void *)123);
+    int *test2 = reinterpret_cast<int *>(123);
+
+    void (*userfun)(const vector<int> &) = user;
+    //void *testPtr = userfun; //error: invalid conversion from ‘void (*)(const std::vector<int>&)’ to ‘void*’ [-fpermissive]
+    return 0;
+}
+
+
+int testArrayParam(uint8_t ta[100]) {
+    cout<<"sizeof(ta) in fun:"<<sizeof(ta)<<" typeid(ta):"<<typeid(ta).name()<<endl;
+    // sizeof(ta) in fun:8 typeid(ta):Ph
+    return ta[0];
+}
+int main(int argc, char *argv[]) {
+    uint8_t testArray[100];
+    uint8_t testArray2[100];
+    // testArray2 = testArray; // error: invalid array assignment
+    // uint8_t &rtestArray[100] = testArray; // error: declaration of ‘rtestArray’ as array of references
+    // uint8_t (&rtestArray)[] = testArray; // error: invalid initialization of reference of type ‘uint8_t (&)[] {aka unsigned char (&)[]}’ from expression of type ‘uint8_t [100] {aka unsigned char [100]}’
+    uint8_t (&rtestArray)[100] = testArray;
+    cout<<"sizeof(ta) in main:"<<sizeof(rtestArray)<<" typeid(ta):"<<typeid(rtestArray).name()<<endl;
+    // sizeof(ta) in main:100 typeid(ta):A100_h
+    testArrayParam(rtestArray);
+    // uint32_t intArray[2] = {1, 2, 3}; // error: too many initializers for ‘uint32_t [2] {aka unsigned int [2]}’
+    char *str = "test string"; // warning: ISO C++ forbids converting a string constant to ‘char*’ [-Wwrite-strings]
+    // str[2] = 'a';
+    const char *rawStr = R"(abcx123\\ "")";
+    const char *rawStr2 = R"*ab(abcx123\\ "")*ab";
+
+    cout << rawStr << endl;
+    cout << rawStr2 << endl;
+
+    const char *testc = "testc";
+    const char *testu8 = u8"testu8";
+    const char *testu8r = u8R"(testu8r)";
+    // const char *testu = u"testu"; // error: cannot convert ‘const char16_t*’ to ‘const char*’ in initialization
+    // const wchar_t *testu1 = u"testu"; // error: cannot convert ‘const char16_t*’ to ‘const wchar_t*’ in initialization
+    const char16_t *testu = u"testu";
+    const char32_t *testU = U"testU";
+    // const char16_t *testlu = Lu"testu"; // ‘Lu’ was not declared in this scope
+    // const char16_t *testul = uL"testu"; // ‘uL’ was not declared in this scope
+    const char16_t *testuR = uR"(testuR)";
+
+    const wchar_t *testw = L"testw";
+    // const char16_t *testwc16 = testw; // error: cannot convert ‘const wchar_t*’ to ‘const char16_t*’ in initialization
+
+}
