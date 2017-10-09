5ad14e4d76b5535d6a06db8279c9a17d44a9517e
chenpeng <chenpeng@voole.com>
2017-10-10 00:34:35 +0800

    update

--- src/Test.cpp
+++ src/Test.cpp
@@ -7,8 +7,10 @@
 #include <iterator>
 #include <cstring>
 #include <iomanip>
+#include <new>
 #include <algorithm>
 #include <type_traits>
+#include <functional>
 
 using namespace std;
 
@@ -27,7 +29,7 @@ void convertTest() {
 }
 
 struct BadClass {
-    BadClass() { throw std::runtime_error(""); }
+    BadClass() { throw std::runtime_error("init failed"); }
 };
 
 enum class AllocatorType { AllocatorX,AllocatorY };
@@ -41,23 +43,83 @@ void operator delete(void *p, AllocatorType type) {
     free(p);
 }
 
-using INT32 = uint32_t;
+void testfunc() {
+    BadClass *a = new(nothrow) BadClass;
+    try {
+        cout<<"try"<<endl;
+    } catch (const std::exception &e) {
+        cout<<e.what()<<" in testfunc"<<endl;
+    }
+    cout<<"finished"<<endl;
+}
+
+void testnothrow() {
+    try {
+        testfunc();
+    } catch (const std::exception &e) {
+        cout<<e.what()<<" in main"<<endl;
+    }
+}
+
+int test_f(int a) { return a; }
+
+int test_initializer() {
+    int v {7};
+    int v2 = {7};
+
+    int testarray[5];
+    // testarray[{1}]; // error: array subscript is not an integer
+    v = {8};
+    v += {99};
+    // v = 7 + {10}; error: initializer list cannot be used on the right hand side of operator '+'
+    test_f({100});
+    return {11};
+}
+
+template<typename C>
+class modulc_printer {
+private:
+    using elementType=typename C::value_type;
+    ostream &os;
+    int m;
+public:
+    modulc_printer(ostream&s,int mm) : os(s), m(mm) {}
+    void operator()(elementType x) const {
+        if (x%m==0) os<<x<<'\n';
+    }
+};
+
+template<typename C>
+void print_modulc(const C& v, ostream &os, int m) {
+    for_each(begin(v), end(v),modulc_printer<typename remove_reference<decltype(v)>::type>(os,m));
+}
+
+template<typename C>
+void print_modulf(const C& v, ostream &os, int m) {
+    for(auto x : v)
+        if (x%m==0) os<<x<<'\n';
+}
+
+template<typename C>
+void print_modull(const C& v, ostream &os, int m) {
+    // error: no viable conversion from '(lambda at /home/chenpeng/Documents/TestCpp/src/Test.cpp:104:29)' to 'int (*)(int)'
+    // 这里编译器会在线生成一个闭包对象类型，因为函数指针无法携带捕获的函数变量等信息，所以lambda不是一个单纯的函数对象
+    // int (*lambdaobj)(int) = [&os,m](int x){ if (x%m==0) os<<x<<'\n'; };
+
+    // error: variable 'lambdaobj' declared with 'auto' type cannot appear in its own initializer
+    // auto lambdaobj = [&os,m,&lambdaobj](int x){ if (x%m==0) os<<x<<'\n'; if(x != 100) lambdaobj(100); };
+
+    // std::function<int(int)> lambdaobj = [&os,m,&lambdaobj](int x)->int { if (x%m==0) os<<x<<'\n'; if(x != 100) lambdaobj(100); return 0; };
+
+    std::function<void(int)> lambdaobj = [&os,m,&lambdaobj](int x) { if (x%m==0) os<<x<<'\n'; if(x != 100) lambdaobj(100); };
+    for_each(begin(v), end(v), lambdaobj);
+}
 
 int main(int argc, char *argv[]) {
-    INT32 reval = 0;
-    try {
-        BadClass *a = new(AllocatorType::AllocatorY) BadClass;
-    } catch (const std::exception &) { }
-
-
-    int *i = new(AllocatorType::AllocatorY) int;
-    delete i;
-    /*
-      Single stepping until exit from function _ZdlPv@plt,
-      which has no line number information.
-      0x00000000004044f0 in ?? ()
-     */
-
+    vector<int> testvec{1,2,3,4,5,6,7,8,9};
+    print_modull(testvec,cout,2);
+    print_modulf(testvec,cout,2);
+    print_modulc(testvec,cout,2);
 }
 
 
